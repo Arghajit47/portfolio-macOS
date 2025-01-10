33be742d93d457380130c6dc05a455f227a77307
Arghajit Singha <arghajitsingha@MacBookAir.lan>
2025-01-10 21:45:55 +0530

    Deploying the portfolio website

--- src/configs/bear.tsx
+++ src/configs/bear.tsx
@@ -107,63 +107,63 @@ const bear: PersonalData[] = [
         icon: "i-ant-design:audio-filled",
         excerpt: "Automated Load Testing using Graphana K6...",
         link: "https://github.com/Arghajit47/LoadTesting-k6"
-      },
-      {
-        id: "pcalg-py",
-        title: "PC Algorithm",
-        file: "https://raw.githubusercontent.com/Renovamen/pcalg-py/master/README.md",
-        icon: "i-tabler:atom-2",
-        excerpt: "Implement PC algorithm in Python...",
-        link: "https://github.com/Renovamen/pcalg-py"
-      },
-      {
-        id: "midgard",
-        title: "Midgard",
-        file: "https://raw.githubusercontent.com/Renovamen/midgard/master/README.md",
-        icon: "i-bx:game",
-        excerpt: "An interactive version of my resume, powered by Vue...",
-        link: "https://resume.zxh.io/"
-      },
-      {
-        id: "image-captioning",
-        title: "Image Captioning",
-        file: "https://raw.githubusercontent.com/Renovamen/Image-Captioning/master/README.md",
-        icon: "i-heroicons-solid:fire",
-        excerpt: "PyTorch implementation of image captioning models...",
-        link: "https://github.com/Renovamen/Image-Captioning"
-      },
-      {
-        id: "wordle-helper",
-        title: "Wordle Helper",
-        file: "https://raw.githubusercontent.com/Renovamen/wordle-helper/main/README.md",
-        icon: "i-bi:grid-1x2",
-        excerpt: "Help you solve Wordle puzzles...",
-        link: "https://wordle.zxh.io"
-      },
-      {
-        id: "cube-solver",
-        title: "Cube Solver",
-        file: "https://raw.githubusercontent.com/Renovamen/Just-a-Cube/master/README.md",
-        icon: "i-bx:cube",
-        excerpt: "A rubik's cube solver (layer-by-layer & two-phase)...",
-        link: "https://cube.zxh.io"
-      },
-      {
-        id: "alkaid",
-        title: "Alkaid",
-        file: "https://raw.githubusercontent.com/Renovamen/alkaid/main/README.md",
-        icon: "i-simple-icons:stellar",
-        excerpt: "Minimal reinforcement-learning toolbox for PyTorch...",
-        link: "https://github.com/Renovamen/alkaid"
-      },
-      {
-        id: "fishmail",
-        title: "Fishmail",
-        file: "https://raw.githubusercontent.com/Renovamen/Fishmail/master/README.md",
-        icon: "i-game-icons:fish-escape",
-        excerpt: "上班的时候装作在 Gmail 上查邮件的样子看知乎摸鱼...",
-        link: "https://fishmail.vercel.app"
       }
+      // {
+      //   id: "pcalg-py",
+      //   title: "PC Algorithm",
+      //   file: "https://raw.githubusercontent.com/Renovamen/pcalg-py/master/README.md",
+      //   icon: "i-tabler:atom-2",
+      //   excerpt: "Implement PC algorithm in Python...",
+      //   link: "https://github.com/Renovamen/pcalg-py"
+      // },
+      // {
+      //   id: "midgard",
+      //   title: "Midgard",
+      //   file: "https://raw.githubusercontent.com/Renovamen/midgard/master/README.md",
+      //   icon: "i-bx:game",
+      //   excerpt: "An interactive version of my resume, powered by Vue...",
+      //   link: "https://resume.zxh.io/"
+      // },
+      // {
+      //   id: "image-captioning",
+      //   title: "Image Captioning",
+      //   file: "https://raw.githubusercontent.com/Renovamen/Image-Captioning/master/README.md",
+      //   icon: "i-heroicons-solid:fire",
+      //   excerpt: "PyTorch implementation of image captioning models...",
+      //   link: "https://github.com/Renovamen/Image-Captioning"
+      // },
+      // {
+      //   id: "wordle-helper",
+      //   title: "Wordle Helper",
+      //   file: "https://raw.githubusercontent.com/Renovamen/wordle-helper/main/README.md",
+      //   icon: "i-bi:grid-1x2",
+      //   excerpt: "Help you solve Wordle puzzles...",
+      //   link: "https://wordle.zxh.io"
+      // },
+      // {
+      //   id: "cube-solver",
+      //   title: "Cube Solver",
+      //   file: "https://raw.githubusercontent.com/Renovamen/Just-a-Cube/master/README.md",
+      //   icon: "i-bx:cube",
+      //   excerpt: "A rubik's cube solver (layer-by-layer & two-phase)...",
+      //   link: "https://cube.zxh.io"
+      // },
+      // {
+      //   id: "alkaid",
+      //   title: "Alkaid",
+      //   file: "https://raw.githubusercontent.com/Renovamen/alkaid/main/README.md",
+      //   icon: "i-simple-icons:stellar",
+      //   excerpt: "Minimal reinforcement-learning toolbox for PyTorch...",
+      //   link: "https://github.com/Renovamen/alkaid"
+      // },
+      // {
+      //   id: "fishmail",
+      //   title: "Fishmail",
+      //   file: "https://raw.githubusercontent.com/Renovamen/Fishmail/master/README.md",
+      //   icon: "i-game-icons:fish-escape",
+      //   excerpt: "上班的时候装作在 Gmail 上查邮件的样子看知乎摸鱼...",
+      //   link: "https://fishmail.vercel.app"
+      // }
     ]
   }
 ];
