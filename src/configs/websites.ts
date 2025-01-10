3b3e2e3b84eca7c964d9689d8165e1ab03cd0ee0
Arghajit Singha <arghajitsingha@MacBookAir.lan>
2025-01-10 23:52:47 +0530

    Updating Safari WebApp

--- src/configs/websites.ts
+++ src/configs/websites.ts
@@ -2,50 +2,38 @@ import type { WebsitesData } from "~/types";
 
 const websites: WebsitesData = {
   favorites: {
-    title: "SNS Links",
+    title: "Suggestions",
     sites: [
       {
-        id: "my-blog",
+        id: "my-resume",
         title: "Website",
-        img: "https://zxh.me/img/avatar.jpg",
-        link: "https://zxh.me/",
+        img: "img/icons/launchpad/resume.png",
+        link: "https://arghajit47.github.io/Arghajit47/",
         inner: true
       },
       {
         id: "my-github",
         title: "Github",
         img: "img/sites/github.svg",
-        link: "https://github.com/Renovamen"
+        link: "https://github.com/Arghajit47"
       },
       {
         id: "my-linkedin",
         title: "Linkedin",
         img: "img/sites/linkedin.svg",
-        link: "https://www.linkedin.com/in/xiaohan-zou"
-      },
-      {
-        id: "my-google-scholar",
-        title: "Scholar",
-        img: "img/sites/google-scholar.svg",
-        link: "https://scholar.google.com/citations?user=RuW6xgMAAAAJ"
-      },
-      {
-        id: "my-zhihu",
-        title: "知乎",
-        img: "img/sites/zhihu.jpeg",
-        link: "https://www.zhihu.com/people/chao-neng-gui-su"
-      },
-      {
-        id: "my-twitter",
-        title: "Twitter",
-        img: "img/sites/twitter.svg",
-        link: "https://www.twitter.com/renovamen_zxh"
+        link: "https://www.linkedin.com/in/arghajit-singha-1646341bb/"
       },
       {
         id: "my-email",
         title: "Email",
         img: "img/sites/gmail.svg",
-        link: "mailto:renovamenzxh@gmail.com"
+        link: "mailto:arghajitsingha47@gmail.com"
+      },
+      {
+        id: "medium",
+        title: "Medium",
+        img: "img/sites/medium.svg",
+        link: "https://medium.com/@arghajitsingha47"
       }
     ]
   },
@@ -59,10 +47,10 @@ const websites: WebsitesData = {
         link: "https://github.com/"
       },
       {
-        id: "arxiv",
-        title: "arXiv",
-        img: "img/sites/arxiv.png",
-        link: "https://arxiv.org/"
+        id: "youtube",
+        title: "Youtube",
+        img: "img/sites/youtube.svg",
+        link: "https://youtube.com/"
       },
       {
         id: "twitter",
@@ -71,10 +59,10 @@ const websites: WebsitesData = {
         link: "https://www.twitter.com/"
       },
       {
-        id: "dribbble",
-        title: "Dribbble",
-        img: "img/sites/dribbble.svg",
-        link: "https://dribbble.com/"
+        id: "medium",
+        title: "Medium",
+        img: "img/sites/medium.svg",
+        link: "https://medium.com/"
       },
       {
         id: "pinterest",
@@ -83,82 +71,16 @@ const websites: WebsitesData = {
         link: "https://www.pinterest.com/"
       },
       {
-        id: "art-station",
-        title: "ArtStation",
-        img: "img/sites/artstation.svg",
-        link: "https://www.artstation.com//"
-      },
-      {
-        id: "bilibili",
-        title: "Bilibili",
-        img: "img/sites/bilibili.svg",
-        link: "https://www.bilibili.com/"
-      },
-      {
-        id: "zhihu",
-        title: "知乎",
-        img: "img/sites/zhihu.jpeg",
-        link: "https://www.zhihu.com/"
-      },
-      {
         id: "leetcode",
         title: "LeetCode",
         img: "img/sites/leetcode.svg",
-        link: "https://leetcode.com/"
+        link: "https://leetcode.com/u/arghajitsingha47/"
       },
       {
         id: "reddit",
         title: "Reddit",
         img: "img/sites/reddit.svg",
         link: "https://www.reddit.com/"
-      },
-      {
-        id: "hacker-news",
-        title: "Hacker News",
-        img: "img/sites/hacker.svg",
-        link: "https://news.ycombinator.com/"
-      },
-      {
-        id: "v2ex",
-        title: "V2EX",
-        img: "https://www.v2ex.com/apple-touch-icon.png",
-        link: "https://www.v2ex.com/"
-      },
-      {
-        id: "aideadline",
-        title: "AI Deadlines",
-        img: "https://aideadlin.es/static/img/favicon.png",
-        link: "https://aideadlin.es/",
-        inner: true
-      },
-      {
-        id: "oh-my-cv",
-        title: "OhMyCV",
-        img: "https://ohmycv.app/apple-touch-icon.png",
-        link: "https://ohmycv.app/"
-      },
-      {
-        id: "steam",
-        title: "Steam",
-        img: "img/sites/steam.svg",
-        link: "https://store.steampowered.com/"
-      },
-      {
-        id: "nazo",
-        title: "Nazo",
-        link: "https://nazo.one-story.cn"
-      },
-      {
-        id: "svgomg",
-        title: "SVGOMG",
-        img: "https://jakearchibald.github.io/svgomg/imgs/icon.png",
-        link: "https://jakearchibald.github.io/svgomg/"
-      },
-      {
-        id: "tiny-png",
-        title: "Tiny PNG",
-        img: "https://tinypng.com/images/panda-chewing-2x.png",
-        link: "https://tinypng.com/"
       }
     ]
   }
